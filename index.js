fd783d8809d4231c526d589da1fb17416123c08f
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2019-08-12 16:01:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const app = express();
+
+passport.use(new GoogleStrategy());
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
