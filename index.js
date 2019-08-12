62a3fca2c95067da695226b1a01068cd490daf2a
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2019-08-12 22:42:23 +0200

    User schema

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require("cookie-session")
+const passport  = require("passport")
+
+const keys = require('./config/key');
+const app = express();
+const authRoutes = require('./routes/authRoutes');
+
+require('./models/User');
 
 require('./services/passport');
 
-const app = express();
+mongoose.connect(keys.MONGODB_ATLAS, {useNewUrlParser: true});
 
-const authRoutes = require("./routes/authRoutes")
+app.use(cookieSession({maxAge: 30 * 24 * 60 *60 *1000, keys:[keys.cookieKey]}))
 
-authRoutes(app)
+app.use(passport.initialize())
+
+app.use(passport.session())
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,23 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const mongoose = require('mongoose');
+const cookieSession = require("cookie-session")
 const keys = require('../config/key');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (e) {
+    console.log(e);
+  }
+});
 passport.use(
   new GoogleStrategy(
     {
@@ -9,8 +25,17 @@ passport.use(
       clientSecret: keys.OAUTH_CLIENT_SECRET,
       callbackURL: '/auth/google/callback',
     },
-    (accessToken, refreshToken, profile,done) => {
-      console.log(accessToken);
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const existingUser = await User.findOne({ googleId: profile.id });
+        if (!existingUser) {
+          const newUser = await new User({ googleId: profile.id }).save();
+          done(null, newUser);
+        }
+        done(null, existingUser);
+      } catch (e) {
+        console.log(e);
+      }
     },
   ),
 );
